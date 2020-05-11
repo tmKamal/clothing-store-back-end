b6d0696559b951345329c0a3009e326359a8e124
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-05-11 19:39:12 +0530

    MongoDB Added.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express =require('express');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app=express();
 
-const PORT= process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}...`)
-})
+
+
+
+mongoose
+  .connect('mongodb+srv://crhunter:Pass4mongodb@cluster0-g3mcz.mongodb.net/clothing-store?retryWrites=true&w=majority')
+  .then(()=>{
+    app.listen(8000);
+    console.log('server & db are up and running!!!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  
